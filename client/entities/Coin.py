476c680c96d9502496ec15a8a1eb2b893c43ea08
gordenfl <gordenfl@gmail.com>
2025-11-09 17:58:06 -0800

    金币碰撞

--- client/entities/Coin.py
+++ client/entities/Coin.py
@@ -10,8 +10,24 @@ class Coin(EntityBase):
         self.spriteCollection = spriteCollection
         self.animation = copy(self.spriteCollection.get("coin").animation)
         self.type = "Item"
+        width = 24
+        height = 24
+        try:
+            frame = self.animation.image
+            if frame:
+                rect = frame.get_rect()
+                width, height = rect.width, rect.height
+        except AttributeError:
+            pass
+        tile_left = x * 32
+        tile_bottom = (y + 1) * 32
+        self.rect.width = width
+        self.rect.height = height
+        self.rect.centerx = tile_left + 16
+        self.rect.bottom = tile_bottom
 
     def update(self, cam):
         if self.alive:
             self.animation.update()
-            self.screen.blit(self.animation.image, (self.rect.x + cam.x, self.rect.y))
+            draw_x = self.rect.x + cam.x
+            self.screen.blit(self.animation.image, (draw_x, self.rect.y))
